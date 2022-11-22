77652dc6ca690b6203f67c3e7b7d0d9523840093
Hung <kwonghung.yip@gmail.com>
2022-11-22 20:52:16 +0000

    Add junit test cases and updating to 2 replicas

--- springboot3-webflux/src/main/java/org/hung/Springboot3WebfluxApplication.java
+++ springboot3-webflux/src/main/java/org/hung/Springboot3WebfluxApplication.java
@@ -1,10 +1,15 @@
 package org.hung;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 public class Springboot3WebfluxApplication {
@@ -15,11 +20,19 @@ public class Springboot3WebfluxApplication {
 
 }
 
+@Slf4j
 @RestController
 class EchoController {
 
 	@GetMapping("/echo")
 	public String echo(@RequestParam String name) {
+		String hostname;
+		try {
+			hostname = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			hostname = "unknown";
+		}
+		log.info("{}:incoming request form {}",hostname,name);
 		return "Hello " + name;
 	}
 }
--- springboot3-webflux/src/test/java/org/hung/Springboot3WebfluxApplicationTests.java
+++ springboot3-webflux/src/test/java/org/hung/Springboot3WebfluxApplicationTests.java
@@ -1,13 +1,31 @@
 package org.hung;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@WebFluxTest(controllers = {EchoController.class})
 class Springboot3WebfluxApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private WebTestClient webClient;
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"john","peter","Mary"})
+	void testEcho(String name) {
+		webClient.get()
+			.uri(uriBuilder -> uriBuilder.path("/echo")
+					.queryParam("name", name)
+					.build())
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody(String.class)
+			.value(string -> assertThat(string).isEqualTo("Hello " + name));
+
 	}
 
 }
